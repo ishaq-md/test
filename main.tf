9c42b178153783250b200f7290632904ccd315f3
unknown <ishaqmd@live.com>
2019-11-14 09:47:52 +0530

    third commit

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_storage_bucket" "terraform_state" {
+  name          = "terraform_bkt"
+  force_destroy = true
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
